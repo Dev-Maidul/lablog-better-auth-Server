798ce9c46d8e71aa79bae34160e4a1658fa11636
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-01-10 02:27:58 +0800

    create api for email-pass sign in and sign out

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ export const  auth=betterAuth({
      database: prismaAdapter(prisma, {
         provider: "postgresql", 
     }),
+    trustedOrigins:[process.env.FRONTEND_URL!],
     emailAndPassword:{
         enabled:true
     }
