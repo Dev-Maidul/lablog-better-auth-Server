d9f6cda9505b3b8939c6548bac779fcd67ad719a
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-01-11 08:15:14 +0800

    authorize github

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,6 +15,7 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      redirectURI:`${process.env.FRONTEND_URL}/api/auth/callback/github`
     },
   },
 });
